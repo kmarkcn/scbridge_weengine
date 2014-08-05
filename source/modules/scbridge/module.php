da45c5fe687995f27f08e6cfd2264c4c7facf5e8
terry <terry@TerryPc>
2014-08-05 18:28:03 +0800

    加载了会议预定页面，修改了一些bug,另外修改了room表max_number,min_number的类型为int.

--- source/modules/scbridge/module.php
+++ source/modules/scbridge/module.php
@@ -78,18 +78,76 @@ class ScbridgeModule extends WeModule {
 		
 	}
 	
+	public function dohotel_contents(){
+		global $_W,$_GPC;
+		$h_id=$_GPC['h_id'];
+		$sql="SELECT * FROM ".tablename('hotel');
+		$hotel=pdo_fetch($sql);
+		switch ($hotel['level']){
+			case 1:
+				$hotel['level']='一星级';
+				break;
+			case 2:
+				$hotel['level']='二星级';
+				break;
+			case 3:
+				$hotel['level']='三星级';
+				break;
+			case 4:
+				$hotel['level']='四星级';
+				break;
+			case 5:
+				$hotel['level']='五星级';
+				break;
+		}
+		$sql_2="SELECT min(price_vip) as pri,max(price_normal) as pr FROM".tablename('hotel_room')."where hotel_id= ".$h_id ." and is_meeting=1";
+		$ho=pdo_fetch($sql_2);
+		$hotel['pr_min']=$ho['pri'];
+		$hotel['pr_max']=$ho['pr'];
+		$sql_3="SELECT min(min_number) as pri,max(max_number) as pr FROM".tablename('hotel_room')."where hotel_id= ".$h_id ." and is_meeting=1";
+		$ho=pdo_fetch($sql_3);
+		$hotel['num_min']=$ho['pri'];
+		$hotel['num_max']=$ho['pr'];
+		include $this->template('scbridge:hotel_re');
+	
+	}
+	
 	public function dohotel_booking(){
 		global $_W,$_GPC;
 		$h_id=$_GPC['h_id'];
-		
 		include $this->template('scbridge:register');
-		
 	}
 	
 	public function domeeting_center(){
 		global $_W,$_GPC;
-		print_r('这个页面还没有做.');
-		//include $this->template('scbridge:')
+		//这里要选出哪些酒店有会议室，然后列出来
+		$sql="select h.name,h.level,h.icon,h.address,h.id  from ims_hotel as h join ims_hotel_room as r on h.id=r.hotel_id where r.is_meeting=1 ";
+		$hotels=pdo_fetchall($sql);
+		//循环遍历
+		for($i=0;$i<count($hotels);$i++){
+			$sql="select max(r.max_number) as max_num from  ims_hotel_room as r  where r.hotel_id = ".$hotels[$i]['id']." and r.is_meeting=1";
+			$re=pdo_fetch($sql);
+			$hotels[$i]['max_num']=$re['max_num'];
+			switch ($hotels[$i]['level']){
+				case 1:
+					$hotels[$i]['level']='一星级';
+					break;
+				case 2:
+					$hotels[$i]['level']='二星级';
+					break;
+				case 3:
+					$hotels[$i]['level']='三星级';
+					break;
+				case 4:
+					$hotels[$i]['level']='四星级';
+					break;
+				case 5:
+					$hotels[$i]['level']='五星级';
+					break;
+			}
+		}
+		
+		include $this->template('scbridge:meeting-reserve');
 	}
 	
 	
@@ -104,6 +162,7 @@ class ScbridgeModule extends WeModule {
 			include $this->template('scbridge:member_center');
 		}
 	}
+	
 	
 	public function domember_rigister() {
 		global $_W, $_GPC;
